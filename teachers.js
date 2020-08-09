d71ea9a97c1f65197f54cc7b93d9955d6c17cddc
Poliana Rufatto <poliana.rufatto@softplan.com.br>
2020-08-09 10:49:54 -0300

    Desafio 4-5: HTTP: PUT e DELETE & Desafio 4-6: Listagem de professores

--- teachers.js
+++ teachers.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 const data = require('./data.json');
 const { age, graduation, date } = require('./utils');
 
+
+// index
+exports.index = function(req, res) {
+    return res.render("teachers/index", { teachers: data.teachers });
+}
+
 // show
 exports.show = function(req, res) {
     const { id } = req.params;
@@ -56,11 +62,9 @@ exports.post = function(req, res) {
         if (err) return res.send("Write file error");
         return res.redirect("/teachers");
     });
-
-    // return res.send(req.body);
 }
 
-// 
+// edit
 exports.edit = function(req, res) {
     const { id } = req.params;
     
@@ -76,4 +80,50 @@ exports.edit = function(req, res) {
     }
 
     return res.render('teachers/edit', { teacher });
+}
+
+// put
+exports.put = function(req, res) {
+    const { id } = req.body;
+    let index = 0;
+
+    const foundTeacher = data.teachers.find(function(teacher, foundIndex) {
+        if (id == teacher.id) {
+            index = foundIndex;
+            return true;
+        }
+    });
+
+    if (!foundTeacher) return res.send("teacher not found");
+
+    const teacher = {
+        ...foundTeacher,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.teachers[index] = teacher;
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) return res.send("Write error!")
+
+        return res.redirect(`/teachers/${id}`)
+    });
+}
+
+// delete
+exports.delete = function(req, res) {
+    const { id } = req.body;
+
+    const filteredTeachers = data.teachers.filter(function(teacher) {
+        return teacher.id != id;
+    });
+
+    data.teachers = filteredTeachers;
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) return res.send("Write file error!");
+
+        return res.redirect("teachers");
+    });
 }
